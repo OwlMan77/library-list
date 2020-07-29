5b4f30a8620ff090e8bae59bf92f1f3b099a9261
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2020-07-29 07:36:01 +0100

    Created recoil store for books with a selector for the current books being viewed and the number of pages that will be needed to display them all.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RecoilRoot } from 'recoil';
+
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router> 
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
